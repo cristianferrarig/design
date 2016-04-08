5c5f3c674d3a1176b1a585d70443de5ee6aeaa51
Cristian Ferrari <cristianferrarig@gmail.com>
2016-04-08 13:18:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SoSafe Website
+
+#### Cristian Ferrari
+
+### LICENSE
+
+[Licensed under MIT.](./license.md) 2014
+
+https://developers.google.com/maps/documentation/javascript/reference#MapTypeStyleFeatureType
---
+++ config.rb
@@ -0,0 +1,53 @@
+activate :autoprefixer
+activate :directory_indexes
+activate :syntax, :line_numbers => true
+
+ready do
+  sprockets.append_path 'app/assets'
+  sprockets.append_path File.join root, 'vendor/bower'
+  # sprockets.import_asset 'jquery'
+  # sprockets.import_asset 'hashgrid'
+end
+
+set :source,       "source"
+set :build_dir,    "public"
+
+set :data_dir,     "data"
+set :locales_dir,  "locales"
+set :helpers_dir,  "helpers"
+
+# relative to source
+set :layouts_dir,  "layouts"
+set :partials_dir, "partials"
+
+set :css_dir,      "assets/stylesheets"
+set :js_dir,       "assets/javascripts"
+set :images_dir,   "assets/images"
+set :fonts_dir,    "assets/fonts"
+
+set :layout,       "default"
+
+# page '/*', layout: "default"
+
+configure :development do
+  activate :livereload
+  set :debug_assets, true
+end
+
+# set :relative_links, false
+# set :show_exceptions, true
+
+configure :build do
+  # activate :imageoptim
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  # activate :asset_hash
+  # activate :relative_assets
+  # set :relative_links, false
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+end
